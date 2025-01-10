9b2118a404534d6f3f13e4965eb71d12c5bb0338
Dilanka Perera <dilankaper@gmail.com>
2025-01-11 00:51:44 +0530

    Added header and footer

---
+++ src/app/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex-grow bg-gradient-to-r from-slate-600 via-slate-800 to-slate-950 text-white p-4">
+      <p className="text-center">© ZynoraX 2025. All rights reserved.</p>
+    </footer>
+  );
+}
