bc2251cee49c5d953111108b3592eebcc949ef28
Dilanka Perera <dilankaper@gmail.com>
2025-01-11 19:14:00 +0530

    Added homepage carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Carousel from "./Carousel";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <div className="max-w-[1280px] mx-auto min-h-screen">
+      <div className="py-8">
+        <Carousel />
+      </div>
+    </div>
   );
 }
