9b6dc7cff53807ed63b1b9923e5020131c23d9e1
Dilanka Perera <dilankaper@gmail.com>
2025-01-21 23:59:39 +0530

    Fixed 404

---
+++ src/app/(main)/[...not-found]/page.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import FadeInWrapper from "../FadeInWrapper";
+import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404: Page not found – ZynoraX",
+  description:
+    "Welcome to ZynoraX, where innovation meets excellence. We are a forward-thinking AI and Web Development company dedicated to empowering businesses with cutting-edge technology solutions that drive growth and success.",
+};
+
+export default function NotFound() {
+  return (
+    <FadeInWrapper>
+      <div className="flex items-center justify-center text-center p-4">
+        <div className="p-8 max-w-lg">
+          <h1 className="text-4xl font-semibold text-gray-100 mb-4 text-center">
+            Page Not Found
+          </h1>
+          <p className="text-lg text-gray-100 mb-2 text-center">
+            The page you&apos;re looking for doesn&apos;t exist or has been
+            moved.
+          </p>
+          <Image
+            src="/404.png"
+            alt="Blog Post Not Found"
+            width={900}
+            height={900}
+            className="mx-auto mb-2"
+          />
+          <p className="text-sm text-gray-100 text-center">
+            You can go back to the{" "}
+            <Link href="/blog" className="text-blue-500">
+              Home Page
+            </Link>{" "}
+            .
+          </p>
+        </div>
+      </div>
+    </FadeInWrapper>
+  );
+}
