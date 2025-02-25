7efc596f8d064e39061460f61a5a5ce680393fc9
Dilanka Perera <dilankaper@gmail.com>
2025-02-25 07:06:48 +0530

    Fixed blog card

--- src/app/BlogNav.tsx
+++ src/app/BlogNav.tsx
@@ -100,27 +100,26 @@ const BlogNav: React.FC<BlogNavProps> = ({ setIsDropdownOpen }) => {
                               <Link
                                 href={`/blog/${category.slug}/${post.slug}`}
                                 className="block"
-                                onClick={() => setIsDropdownOpen(null)}
                               >
                                 <div className="bg-blue-100 shadow-md overflow-hidden max-w-[300px] justify-center group cursor-pointer transition-transform duration-300 hover:scale-105">
-                                  <div className="relative w-full h-30 overflow-hidden">
+                                  <div className="relative w-full h-50 overflow-hidden">
                                     <Image
                                       src={`https:${post.featuredImage}`}
                                       alt={post.title}
                                       width={1280}
                                       height={720}
-                                      className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+                                      className="w-full h-full object-cover [mask-image:linear-gradient(to_bottom,white_0%,transparent)] [-webkit-mask-image:linear-gradient(to_bottom,white_0%,transparent)] transition-transform duration-300 ease-in-out group-hover:scale-110"
                                       unoptimized
                                     />
                                   </div>
 
-                                  <div className="p-4">
+                                  <div className="-mt-[50px] p-4 relative">
                                     <div className="flex justify-between">
                                       <div className="bg-blue-300 text-xs font-semibold px-3 py-2 rounded-lg">
                                         Blog
                                       </div>
 
-                                      <p className="text-slate-600 font-normal text-sm mt-2">
+                                      <p className="text-slate-600 font-normal text-md mt-2">
                                         {new Date(
                                           post.publishedDate,
                                         ).toLocaleDateString('en-US', {
@@ -130,12 +129,12 @@ const BlogNav: React.FC<BlogNavProps> = ({ setIsDropdownOpen }) => {
                                         })}
                                       </p>
                                     </div>
-                                    <div className="flex flex-col justify-between min-h-[140px] h-[140px] pt-4">
+                                    <div className="flex flex-col justify-between min-h-[150px] h-[150px] pt-4">
                                       <h3 className="text-md font-medium mt-2 line-clamp-3">
                                         {post.title}
                                       </h3>
                                       <div className="flex">
-                                        <span className="text-sm text-slate-600 hover:text-slate-500 font-medium mt-3 flex items-center gap-1">
+                                        <span className="text-slate-600 hover:text-slate-500 font-medium mt-3 flex items-center gap-1">
                                           Learn more <ChevronRight size={18} />
                                         </span>
                                       </div>
--- src/app/blog/BlogCard.tsx
+++ src/app/blog/BlogCard.tsx
@@ -20,18 +20,18 @@ const BlogCard: React.FC<BlogCardProps> = ({ post }) => {
   return (
     <Link href={`/blog/${post.categorySlug}/${post.slug}`} className="block">
       <div className="bg-blue-100 shadow-md overflow-hidden max-w-[400px] justify-center group cursor-pointer transition-transform duration-300 hover:scale-105">
-        <div className="relative w-full h-40 overflow-hidden">
+        <div className="relative w-full h-60 overflow-hidden">
           <Image
             src={`https:${post.featuredImage}`}
             alt={post.title}
             width={1280}
             height={720}
-            className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+            className="w-full h-full object-cover [mask-image:linear-gradient(to_bottom,white_0%,transparent)] [-webkit-mask-image:linear-gradient(to_bottom,white_0%,transparent)] transition-transform duration-300 ease-in-out group-hover:scale-110"
             unoptimized
           />
         </div>
 
-        <div className="p-4">
+        <div className="-mt-20 p-4 relative">
           <div className="flex justify-between">
             <div className="bg-blue-300 text-xs font-semibold px-3 py-2 rounded-lg">
               Blog
